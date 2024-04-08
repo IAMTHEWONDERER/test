e05e52d7fbae388b30695e9adb1dc20e3a8b7c9f
Oussama Alouche <theoblivionitself@gmail.com>
2024-04-08 13:25:57 +0000

    added toggling task, adding task and fetching task plus some basic css as a place holder until i get done with the react code

--- test/src/App.js
+++ test/src/App.js
@@ -6,6 +6,49 @@ function App(){
   const [taskName, setTaskName]=useState('');
   
   useEffect(()=>{
-    fetch
-  })
+    fetchTasks();
+  },[])
+  const fetchTasks =()=>{
+    fetch('http://localhost:3001/tasks')
+    .then(response => response.json())
+    .then(data => setTasks(data));
+  }
+  const addTask = () =>{
+    if(taskName.trim()!== ''){
+      fetch('http://localhost:3001/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: taskName, finished: false
+        })
+      })
+      .then(response => response.json())
+      .then(data => {
+        setTasks([...tasks, data]);
+        setTaskName('');
+      });
+    }
+  }
+  const toggleTask = (id) => {
+    const updateTasks = [...tasks];
+    const index = updateTasks.findIndex(task => task.id === id);
+    if (index !== -1) {
+      updatedTasks[index].done = !updateTasks[index].done;
+      fetch(`http://localhost:3001/tsks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateTasks[index]),
+      })
+        .then(() => setTasks(updateTasks));
+    }
+  };
+
+
+
+
+
 }
